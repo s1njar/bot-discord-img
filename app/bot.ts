f5276545528fbf70a73155930aa84f216e10e1ce
s1njar <s1njar.mail@gmail.com>
2021-04-23 18:15:19 +0200

    [TASK] Added new help command to bot

--- app/bot.ts
+++ app/bot.ts
@@ -4,12 +4,14 @@ import {IEvent} from "../src/core/event/IEvent";
 import {Container} from "typescript-ioc";
 import {ImageEvent} from "../src/image/event/ImageEvent";
 import {BackupEvent} from "../src/image/event/BackupEvent";
+import {HelpEvent} from "../src/image/event/HelpEvent";
 
 const app = new App();
 
 const events: IEvent[] = [
     Container.get(ImageEvent),
-    Container.get(BackupEvent)
+    Container.get(BackupEvent),
+    Container.get(HelpEvent)
 ];
 
 const serviceProvider: IServiceProvider[] = [

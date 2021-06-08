c0ec488b1ebae05279f13a4bf4ce4bca30bb6515
s1njar <s1njar.mail@gmail.com>
2021-06-08 23:50:53 +0200

    [TASK] Changed server ip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
   deploy: {
     production: {
       user : 'root',
-      host : '51.38.213.135',
+      host : '159.69.28.12',
       ref  : 'origin/master',
       repo : 'git@gitlab.com:ares-only/back-boner-bot.git',
       path : '/var/app/ares-back-boner',
